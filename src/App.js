2a79fa0c4c8fa62cf3b34583eec49b29c6ba4f24
Divyansh <krdivyansh2596@gmail.com>
2021-03-25 03:05:08 +0530

    removed unused declarations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react' ;
+import React from 'react' ;
 import './App.css';
 import CharacterList from './Components/CharacterList';
 import HeroList from './Components/HeroList';
